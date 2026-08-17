215fa2e9afb829e19139756cc1a90338aa117a72
Rahul <rahulg1507@gmail.com>
2026-08-17 15:03:21 +0800

    Rename chatbot to Nova

---
+++ src/main/java/Nova.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Nova chatbot application.
+ */
+public class Nova {
+    public static void main(String[] args) {
+        String banner = " _   _    ___    _   _    _\n"
+                + "| \\ | |  / _ \\  | | | |  / \\\n"
+                + "|  \\| | | | | | | | | | / _ \\\n"
+                + "| |\\  | | |_| |  \\ V / / ___ \\\n"
+                + "|_| \\_|  \\___/    \\_/ /_/   \\_\\\n";
+        System.out.println(banner);
+    }
+}
