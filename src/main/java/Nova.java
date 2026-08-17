d74606f78cacbf43ab4976ffb432cad6299df57a
Rahul <rahulg1507@gmail.com>
2026-08-17 15:11:31 +0800

    Add greeting and exit message

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -3,11 +3,19 @@
  */
 public class Nova {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = " _   _    ___    _   _    _\n"
                 + "| \\ | |  / _ \\  | | | |  / \\\n"
                 + "|  \\| | | | | | | | | | / _ \\\n"
                 + "| |\\  | | |_| |  \\ V / / ___ \\\n"
                 + "|_| \\_|  \\___/    \\_/ /_/   \\_\\\n";
-        System.out.println(banner);
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Nova.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
