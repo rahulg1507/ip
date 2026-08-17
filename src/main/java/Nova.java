d4ee797819c6dc05720bee7c320a60393640591d
Rahul <rahulg1507@gmail.com>
2026-08-17 15:25:27 +0800

    Add support for storing and listing tasks

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -18,6 +18,8 @@ public class Nova {
         System.out.println("What can I do for you?");
         System.out.println(divider);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -29,7 +31,16 @@ public class Nova {
                 break;
             }
 
-            System.out.println(" " + command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
+            }
+
             System.out.println(divider);
         }
     }
