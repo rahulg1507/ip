b399f9f6af4e9803df5b45d4abe6bb0d16401ebf
Rahul <rahulg1507@gmail.com>
2026-08-17 16:07:43 +0800

    Add support for marking tasks as done

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -19,6 +19,7 @@ public class Nova {
         System.out.println(divider);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
@@ -32,9 +33,17 @@ public class Nova {
             }
 
             if (command.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "[X] " : "[ ] ";
+                    System.out.println(" " + (i + 1) + "." + status + tasks[i]);
                 }
+            } else if (command.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(command.substring(5));
+                int taskIndex = taskNumber - 1;
+                isDone[taskIndex] = true;
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [X] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = command;
                 taskCount++;
